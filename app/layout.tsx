d485d2cae9acc3f8fe5b5ada6adf3996509f1b72
Johnson Victory <johnsonvictory911@gmail.com>
2026-01-08 16:23:05 +0100

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 // import { Inter } from "next/font/google"; 
 import "./globals.css";
 import { ClientLayout } from "@/components/layout/ClientLayout";
@@ -53,6 +54,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-bg-dark min-h-screen text-text antialiased">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z8QW2ELPPB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Z8QW2ELPPB');
+          `}
+        </Script>
         <JsonLd />
         <ClientLayout>
           {children}
